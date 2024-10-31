7b55526aae52a74f472cfc4928a5f6be7866b63e
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2024-11-01 02:52:00 +0500

    add product page and create product

--- front/src/pages/all-product-page/all-product-page.tsx
+++ front/src/pages/all-product-page/all-product-page.tsx
@@ -20,7 +20,17 @@ export default function AllProductPage(): JSX.Element {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
         price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
+        image: 'https://zadachi-po-khimii.ru/wp-content/uploads/2015/01/apple.jpg'
+    },  {
+        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
+        name: 'test',
+        price: 100,
+        image: 'https://static.miraheze.org/mylittleponywiki/thumb/b/b5/S9E26_Applejack.png/270px-S9E26_Applejack.png'
+    },  {
+        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
+        name: 'test',
+        price: 10,
+        image: 'https://pogrebokpro.ru/GetImageFromTable.ashx?TableName=dir_goods&Id=10830&size=full'
     },  {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
@@ -30,7 +40,12 @@ export default function AllProductPage(): JSX.Element {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
         price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
+        image: 'https://himpitertorg.ru/image/cache/catalog/products/otdushki/zelenoejablokootdushka-900x900.jpg'
+    },  {
+        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
+        name: 'test',
+        price: 10,
+        image: 'https://cdn-img.perekrestok.ru/i/800x800-fit/xdelivery/files/a7/5c/6bd10780d88949d10ec6958bd546.jpg'
     },  {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
@@ -40,22 +55,7 @@ export default function AllProductPage(): JSX.Element {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
         price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
-    },  {
-        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
-        name: 'test',
-        price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
-    },  {
-        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
-        name: 'test',
-        price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
-    },  {
-        id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
-        name: 'test',
-        price: 10,
-        image: 'https://st24.stpulscen.ru/images/product/217/941/930_original.jpg'
+        image: 'https://delivery.syemslona.ru/up/catalog/detail/45009.jpg'
     },  {
         id: '95c4430b-c5f7-4f99-8bd8-17c428704dca',
         name: 'test',
--- front/src/pages/create-product/create-product-page.tsx
+++ front/src/pages/create-product/create-product-page.tsx
@@ -3,18 +3,123 @@ import { Link } from "react-router-dom"
 import { Helmet } from "react-helmet-async"
 
 export default function CreateProductPage(): JSX.Element {
-  return (
-    <>
+    return (
+        <>
 
 
 
-          <main className="main">
+            <main className="main">
 
-              Ознакомиться с публичным swagger нашего проекта и тем самым узнать больше о функционале backend-приложения 
-            
+                <h1>Product Constructor</h1>
+                <div className="constructor">
+                <div className="form">
+                    <div className="form-line">
+                        <div>
+                            <label className="label">
+                                Наименование<span className="required-field">*</span>
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                // ref={priceRef}
+                                type="string"
+                                className="field price-field"
+                                placeholder="Введите стоимость в рублях"
+                                required={true}
+                            // defaultValue={newSurvey?.price}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-line">
+                        <div>
+                            <label>
+                                Стоимость<span className="required-field">*</span>
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                // ref={priceRef}
+                                type="number"
+                                className="field price-field"
+                                placeholder="Введите стоимость в рублях"
+                                required={true}
+                            // defaultValue={newSurvey?.price}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-line">
+                        <div>
+                            <label>
+                                Количество<span className="required-field">*</span>
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                // ref={priceRef}
+                                type="number"
+                                className="field price-field"
+                                placeholder="Введите стоимость в рублях"
+                                required={true}
+                            // defaultValue={newSurvey?.price}
+                            />
+                        </div>
+                    </div>
+                    <div className="survey-description">
+                    <div>
+                      <label>Описание</label>
+                    </div>
+                    <div>
+                      <textarea
+                        // ref={descriptionRef}
+                        className="field description-field"
+                        placeholder="Введите описание"
+                        // defaultValue={newSurvey?.description}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-line">
+                        <div>
+                            <label>
+                                Ссылка на изображение<span className="required-field">*</span>
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                // ref={priceRef}
+                                type="string"
+                                className="field price-field"
+                                placeholder="Введите стоимость в рублях"
+                                required={true}
+                            // defaultValue={newSurvey?.price}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-line">
+                        <div>
+                            <label>
+                                Продавец<span className="required-field">*</span>
+                            </label>
+                        </div>
+                        <div>
+                            <input
+                                // ref={priceRef}
+                                type="number"
+                                className="field price-field"
+                                placeholder="Введите стоимость в рублях"
+                                required={true}
+                            // defaultValue={newSurvey?.price}
+                            />
+                        </div>
+                    </div>
+                
+                    <div className="form-line">
+                    <button className="btn">Создать</button>
+                    </div>
+                </div>
 
-          </main>
+        </div>
+            </main>
 
-    </>
-  )
+        </>
+    )
 }
